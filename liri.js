2d21b6c52a367d678d4aa825b2c23295d5b38f94
AnotherMysterySolved <14mprice@gmail.com>
2017-04-05 22:18:28 -0400

    Tweet Spotify IMDB pretty much finished

--- liri.js
+++ liri.js
@@ -49,23 +49,45 @@ function getTweets() {
         }
         console.log("\n", "---~~~Last 20 Tweets~~~---");
         for (var i = 0; i < tweets.length; i++) {
-            console.log("\n", tweets[i].created_at, tweets[i].text);  //'%j \n' took out the %j... still works
+            console.log("\n", tweets[i].created_at, tweets[i].text); //'%j \n' took out the %j... still works
         }
     });
 };
 
 //-----~~~~~searchSong function~~~~~-----
 function searchSong() {
-spotify.search({ type: 'track', query: 'dancing in the moonlight' }, function(err, data) {
-    if ( err ) {
-        console.log('Error occurred: ' + err);
-        return;
-    }
-      console.log("\n", "---~~~Spotify Results~~~---");
-    // for (var i = 0; i < data.length; i++){
-      var path = data.tracks.items[0];
-      console.log("\n", 'Artist(s): ' + path.artists[0].name, "\n",'Album: ' + path.album.name, "\n", 'Song Name: ' + path.name, "\n",'Preview Link: ' + path.preview_url);
-    // }
-
-});
+    var params = {
+        type: "track",
+        query: value || "the sign ace of base"
+    };
+    spotify.search(params, function(err, data) {
+        if (err) {
+            console.log('Error occurred: ' + err);
+            return;
+        }
+        console.log("\n", "---~~~Spotify Results~~~---");
+        // for (var i = 0; i < data.length; i++){
+        var path = data.tracks.items[0];
+        console.log("\n", 'Artist(s): ' + path.artists[0].name, "\n", 'Album: ' + path.album.name, "\n", 'Song Name: ' + path.name, "\n", 'Preview Link: ' + path.preview_url);
+        // }
+    });
 };
+
+//-----~~~~~searchMovie function~~~~~-----
+function searchMovie() {
+    var movie = value || "Mr. Nobody";
+    request("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&r=json", function(error, response, body) {
+        // If there were no errors and the response code was 200 (i.e. the request was successful)
+        if (!error && response.statusCode === 200) {
+            var body = JSON.parse(body);
+            console.log("Title: " + body.Title);
+            console.log("Year Released: " + body.Year);
+            console.log("IMDB Rating: " + body.imdbRating);
+            console.log("Countries: " + body.Country);
+            console.log("Language(s): " + body.Language);
+            console.log("Plot: " + body.Plot);
+            console.log("Actors: " + body.Actors);
+            //console.log("Rotten Tomatos Rating: " + body.Ratings.Source);
+        }
+    });
+}
